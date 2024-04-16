06c10a9b061c9fbd237b392b44b2836e59d0fd0b
creih <iherr.9@gmail.com>
2024-04-17 00:55:33 +0200

    testing the creation function made

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,110 +1,74 @@
-#!/usr/bin/python3
-"""Defines unittests for console.py.
-
-Unittest classes:
-    TestHBNBCommand_prompting
-
-"""
-
 import unittest
-from models import storage
 from unittest.mock import patch
-from models.engine.file_storage import FileStorage
-from console import HBNBCommand
 from io import StringIO
+from console import HBNBCommand
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.__init__ import storage
 
+class TestHBNBCommand(unittest.TestCase):
 
-class TestHBNBCommand_prompting(unittest.TestCase):
-    """Unittests for testing prompting of the HBNB command interpreter."""
+    def setUp(self):
+        self.console = HBNBCommand()
 
-    def test_console_prompt(self):
-        """Test the prompt attribute of the HBNB command interpreter."""
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('create')
+            self.assertEqual('** class name missing **\n', mock_out.getvalue())
 
-    # def test_function_quit(self):
-    #     """Test the do_quit method of the HBNB command interpreter."""
-    #     hbnb_command_instance = HBNBCommand()
-    #     self.assertTrue(hbnb_command_instance.do_quit(None))
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('create MyModel')
+            self.assertEqual('** class doesn\'t exist **\n', mock_out.getvalue())
 
-    # def test_EOF_exits(self):
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertTrue(HBNBCommand().onecmd("EOF"))
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('create BaseModel')
+            self.assertEqual('\n', mock_out.getvalue())
+            self.assertIsInstance(storage.all()["BaseModel." + mock_out.getvalue().strip()], BaseModel)
 
-    # def test_count_State(self):
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("create State"))
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("State.count()"))
+    def test_show(self):
+        storage._FileStorage__objects = {}
+        bm = BaseModel()
+        bm.save()
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('show')
+            self.assertEqual('** class name missing **\n', mock_out.getvalue())
 
-    # def test_create_invalid_syntax(self):
-    #     correct = "*** Unknown syntax: MyModel.create()"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("MyModel.create()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
-    #     correct = "*** Unknown syntax: BaseModel.create()"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("BaseModel.create()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('show MyModel')
+            self.assertEqual('** class doesn\'t exist **\n', mock_out.getvalue())
 
-    # def test_update_missing_class(self):
-    #     correct = "** class name missing **"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("update"))
-    #         self.assertEqual(correct, output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".update()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('show BaseModel')
+            self.assertEqual('** instance id missing **\n', mock_out.getvalue())
 
-    # def test_Show_exits(self):
-    #     correct = "** class name missing **"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("show"))
-    #         self.assertEqual(correct, output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".show()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('show BaseModel {}'.format(bm.id))
+            self.assertEqual(str(bm) + '\n', mock_out.getvalue())
 
-    # def test_destroy_exits(self):
-    #     correct = "** class name missing **"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("destroy"))
-    #         self.assertEqual(correct, output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".destroy()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
+    def test_destroy(self):
+        storage._FileStorage__objects = {}
+        bm = BaseModel()
+        bm.save()
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('destroy')
+            self.assertEqual('** class name missing **\n', mock_out.getvalue())
 
-    # def test_help_functionality(self):
-    #     correct = "*** Unknown syntax: .help()"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("help"))
-    #         self.assertTrue("Documented commands" in output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".help()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('destroy MyModel')
+            self.assertEqual('** class doesn\'t exist **\n', mock_out.getvalue())
 
-    # def test_quit_functionality(self):
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertTrue(HBNBCommand().onecmd("quit"))
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('destroy BaseModel')
+            self.assertEqual('** instance id missing **\n', mock_out.getvalue())
 
-    # def test_all_functionality(self):
-    #     correct = "["
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("all"))
-    #         self.assertTrue(correct in output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".all()"))
-    #         self.assertEqual(correct, output.getvalue().strip())
-
-    # def test_create_functionality(self):
-    #     correct = "** class name missing **"
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd("create"))
-    #         self.assertTrue(correct in output.getvalue().strip())
-    #     with patch("sys.stdout", new=StringIO()) as output:
-    #         self.assertFalse(HBNBCommand().onecmd(".create()"))
-    #         self.assertEqual("*** Unknown syntax: .create()",
-    #                          output.getvalue().strip())
-
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            self.console.onecmd('destroy BaseModel {}'.format(bm.id))
+            self.assertNotIn(bm, storage.all().values())
 
 if __name__ == "__main__":
     unittest.main()
