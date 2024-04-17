db0b401a0b812793aec0d335671e4524cac35947
creih <iherr.9@gmail.com>
2024-04-17 02:52:09 +0200

    task 8 file citty.py

--- models/city.py
+++ models/city.py
@@ -3,10 +3,12 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This class defines a city by various attributes"""
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
