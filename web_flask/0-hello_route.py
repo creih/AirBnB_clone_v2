fd3fc6d357fb9cf32b343b395920d6e71a237e77
creih <iherr.9@gmail.com>
2024-05-18 08:39:35 +0000

    task 1 running

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,17 @@
 """
 this module resolves task 1 per requirements more details in the readme file
 """
-import flask
+from flask import Flask
+
+app = Flask(__name__)
 
 
+
+@app.route('/', strict_slashes=False)
+def murugo():
+    """ this is my home function for task 0"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
