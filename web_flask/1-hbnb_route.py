8aa425bbc3d96844141a1fae3e8bd0cf1b60ade7
creih <iherr.9@gmail.com>
2024-05-18 09:07:28 +0000

    task 1 done

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module resolves task 1 per requirements more details in the readme file
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def murugo():
+    """ this is my home function for task 0. """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def muhanda_1():
+    """ this is the route for hbnb. """
+    return 'HBNB'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
